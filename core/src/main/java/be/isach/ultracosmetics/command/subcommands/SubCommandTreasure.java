13edee55ae09c0199b5d94d760d94381bf21aefa
tavstal <tavstal@gmail.com>
2025-08-25 21:37:11 +0200

    Refactor subcommands to use MessageManager for localization and improve user feedback

--- core/src/main/java/be/isach/ultracosmetics/command/subcommands/SubCommandTreasure.java
+++ core/src/main/java/be/isach/ultracosmetics/command/subcommands/SubCommandTreasure.java
@@ -12,7 +12,6 @@ import be.isach.ultracosmetics.util.BlockUtils;
 import be.isach.ultracosmetics.version.VersionManager;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
@@ -31,18 +30,18 @@ import java.util.List;
 public class SubCommandTreasure extends SubCommand {
 
     public SubCommandTreasure(UltraCosmetics ultraCosmetics) {
-        super("treasure", "Starts Treasure Chest.", "[player] [<x> <y> <z>] [world]", ultraCosmetics);
+        super("treasure", "Commands.Treasure.Description", "Commands.Treasure.Usage", ultraCosmetics);
     }
 
     @Override
     protected void onExeAnyone(CommandSender sender, String[] args) {
         if (args.length < 2 && !(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "You must specify a player.");
+            MessageManager.send(sender, "Commands.Player-Required");
             return;
         }
 
         if (args.length > 6) {
-            sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Incorrect Usage! " + getUsage());
+            badUsage(sender);
             return;
         }
 
@@ -54,7 +53,8 @@ public class SubCommandTreasure extends SubCommand {
         } else {
             opener = Bukkit.getPlayer(args[1]);
             if (opener == null) {
-                sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Player " + args[1] + " not found!");
+                var playerPlaceholder = Placeholder.unparsed("player", args[1]);
+                MessageManager.send(sender, "Commands.Player-Not-Found", playerPlaceholder);
                 return;
             }
         }
@@ -94,7 +94,7 @@ public class SubCommandTreasure extends SubCommand {
             y = Integer.parseInt(args[3]);
             z = Integer.parseInt(args[4]);
         } catch (NumberFormatException e) {
-            sender.sendMessage(ChatColor.RED.toString() + ChatColor.BOLD + "Invalid coordinates!");
+            MessageManager.send(sender, "Invalid-Cords");
             return;
         }
 
@@ -103,7 +103,7 @@ public class SubCommandTreasure extends SubCommand {
         if (args.length > 5) {
             world = Bukkit.getWorld(args[5]);
             if (world == null) {
-                sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "World " + args[5] + " doesn't exist!");
+                MessageManager.send(sender, "Invalid-World");
                 return;
             }
             // form: /uc treasure (player) (x) (y) (z)
